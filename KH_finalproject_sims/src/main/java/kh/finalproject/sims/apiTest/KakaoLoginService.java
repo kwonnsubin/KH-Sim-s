30a9ed2b1a229c19134358aad3266de2fbb170db
Mercury <sooseong98@gmail.com>
2023-03-12 19:02:27 +0900

    카카오 로그인

---
+++ KH_finalproject_sims/src/main/java/kh/finalproject/sims/apiTest/KakaoLoginService.java
@@ -0,0 +1,11 @@
+package kh.finalproject.sims.apiTest;
+
+import java.util.HashMap;
+
+public interface KakaoLoginService {
+	
+	String getAccessToken(String authorize_code) throws Throwable;
+	
+	public HashMap<String, Object> getUserInfo(String access_Token) throws Throwable;
+
+}
